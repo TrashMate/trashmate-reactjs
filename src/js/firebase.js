841cae46a3a4c19ea99709d304e919179969fe54
Yashi Srivastava <65721283+ysriv@users.noreply.github.com>
2021-10-31 00:31:43 +0530

    Update firebase.js

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDbCaoy8BvM4KNfCQlihr1V_ECSV2zAu1U",
-    authDomain: "trashmate-a7b96.firebaseapp.com",
-    databaseURL: "https://trashmate-a7b96.firebaseio.com",
-    projectId: "trashmate-a7b96",
-    storageBucket: "trashmate-a7b96.appspot.com",
-    messagingSenderId: "245505286948",
-    appId: "1:245505286948:web:403370400b17db292549b1",
-    measurementId: "G-JBM2NLEXVF",
+   apiKey: "AIzaSyDbCaoy8BvM4KNfCQlihr1V_ECSV2zAu1U",
+  authDomain: "trashmate-a7b96.firebaseapp.com",
+  databaseURL: "https://trashmate-a7b96.firebaseio.com",
+  projectId: "trashmate-a7b96",
+  storageBucket: "trashmate-a7b96.appspot.com",
+  messagingSenderId: "245505286948",
+  appId: "1:245505286948:web:403370400b17db292549b1",
+  measurementId: "G-JBM2NLEXVF"
 });
 
 const db = firebaseApp.firestore();
